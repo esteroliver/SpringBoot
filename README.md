5dfbe88e6c99dec08bbcb23c1d55aaef9e9935f3
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-21 10:15:00 -0300

    build: projeto para armazenar imagens na aplicação

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SpringBoot
+Repositório para guardar aplicações desenvolvidas para estudar SpringBoot.
+
+## Aplicações
+[Aplicação com armazenamento de imagens](https://github.com/esteroliver/SpringBoot/rest-images)
---
+++ rest-images/src/main/java/com/esteroliver/restimages/RestImagesApplication.java
@@ -0,0 +1,13 @@
+package com.esteroliver.restimages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestImagesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestImagesApplication.class, args);
+    }
+
+}
