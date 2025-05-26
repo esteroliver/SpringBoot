009d940d4eed807a343292ce357f6db4fcf5be82
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-26 11:10:12 -0300

    chore: configuração para uploud de arquivos

--- files/src/main/java/br/com/esteroliver/files/FilesApplication.java
+++ files/src/main/java/br/com/esteroliver/files/FilesApplication.java
@@ -1,9 +1,14 @@
 package br.com.esteroliver.files;
 
+import br.com.esteroliver.files.configs.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageConfig.class
+})
 public class FilesApplication {
 
     public static void main(String[] args) {
