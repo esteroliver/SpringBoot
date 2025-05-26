8b7e14f20db8ac23c7add01f88df8a6b1703e5a1
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-05-26 11:19:22 -0300

    feat: criando exceptions e responses

---
+++ files/src/main/java/br/com/esteroliver/files/exceptions/FileStorageException.java
@@ -0,0 +1,14 @@
+package br.com.esteroliver.files.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class FileStorageException extends Exception{
+     public FileStorageException(String message) {
+         super(message);
+     }
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
