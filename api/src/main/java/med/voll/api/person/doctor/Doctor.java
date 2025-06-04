400bb937ec22d1f21574e2870543b1c8793522ea
oliver.ester.2004 <oliver.ester.2004@gmail.com>
2025-06-04 08:02:31 -0300

    feat: alterações nas entidades para integração com banco de dados

--- api/src/main/java/med/voll/api/person/doctor/Doctor.java
+++ api/src/main/java/med/voll/api/person/doctor/Doctor.java
@@ -1,15 +1,28 @@
 package med.voll.api.person.doctor;
 
+import jakarta.persistence.*;
+
 import lombok.Data;
 import med.voll.api.utils.address.Address;
 
+@Entity
 @Data
+@Table(name = "doctors")
 public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private String nome;
+
     private String email;
+
     private String telefone;
+
     private String crm;
+
+    @ManyToOne
     private Address endereco;
+
     private SpecialtyEnum especialidade;
 }
--- api/src/main/java/med/voll/api/person/patient/Patient.java
+++ api/src/main/java/med/voll/api/person/patient/Patient.java
@@ -1,15 +1,26 @@
 package med.voll.api.person.patient;
 
+import jakarta.persistence.*;
+
 import lombok.Data;
-import med.voll.api.person.doctor.SpecialtyEnum;
 import med.voll.api.utils.address.Address;
 
+@Entity
 @Data
+@Table(name = "patients")
 public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
     private String nome;
+
     private String cpf;
+
     private String email;
+
     private String telefone;
+
+    @ManyToOne
     private Address endereco;
 }
