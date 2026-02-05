de92454775e2d23c18d0e08463dad462d259514b
esteroliver <oliver.ester.2004@gmail.com>
2026-02-04 22:04:39 -0300

    feat: Services de user details e de jwt token

---
+++ auth/src/main/java/br/com/esteroliver/auth/b_application/service/JwtTokenService.java
@@ -0,0 +1,43 @@
+package br.com.esteroliver.auth.b_application.service;
+
+import br.com.esteroliver.auth.a_domain.model.UserDetailsImpl;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+
+@Service
+public class JwtTokenService {
+    private static final String SECRET_KEY = "";
+    private static final String ISSUER = "esteroliver";
+
+    public String gerarToken(UserDetailsImpl userDetails){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY);
+            return JWT.create()
+            .withIssuer(ISSUER)
+            .withSubject(userDetails.getUsername())
+            .withIssuedAt(dataCriacao())
+            .withExpiresAt(dataExpiracao())
+            .sign(algorithm);
+        }
+        catch(JWTCreationException exception){
+            throw new JWTCreationException("Erro ao gerar token.", exception);
+        }
+    }
+
+    private Instant dataCriacao(){
+        return ZonedDateTime.now(ZoneId.of("America/Recife")).toInstant();
+    }
+
+    private Instant dataExpiracao(){
+        return ZonedDateTime.now(ZoneId.of("America/Recife")).plusHours(5).toInstant();
+    }
+}
---
+++ auth/src/main/java/br/com/esteroliver/auth/b_application/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package br.com.esteroliver.auth.b_application.service;
+
+import br.com.esteroliver.auth.a_domain.model.UserDetailsImpl;
+import br.com.esteroliver.auth.a_domain.model.Usuario;
+import br.com.esteroliver.auth.a_domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+        return new UserDetailsImpl(usuario);
+    }
+}
