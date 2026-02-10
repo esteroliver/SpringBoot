5f52c0ea29d6a1cd30bbad99d7fd9d52404e20fe
esteroliver <oliver.ester.2004@gmail.com>
2026-02-09 21:03:37 -0300

    rename: renomeando records

---
+++ auth/src/main/java/br/com/esteroliver/auth/b_application/dto/TokenResponseDTO.java
@@ -0,0 +1,30 @@
+package br.com.esteroliver.auth.b_application.dto;
+
+import br.com.esteroliver.auth.a_domain.enums.Papel;
+import br.com.esteroliver.auth.a_domain.model.UserDetailsImpl;
+import br.com.esteroliver.auth.a_domain.model.Usuario;
+
+public record TokenResponseDTO(
+
+    Long id,
+
+    String email,
+
+    String nome,
+
+    Papel papel,
+
+    String token
+    
+) {
+    public TokenResponseDTO tokenResponse(String token, Usuario usuario){
+
+        return new TokenResponseDTO(
+                    usuario.getId(), 
+                    usuario.getEmail(), 
+                    usuario.getNome(), 
+                    usuario.getPapel(), 
+                    token
+                );
+    } 
+} 
--- auth/src/main/java/br/com/esteroliver/auth/b_application/dto/UsuarioPostDTO.java
+++ auth/src/main/java/br/com/esteroliver/auth/b_application/dto/UsuarioPostDTO.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UsuarioInputDTO(
+public record UsuarioPostDTO(
 
     @Email
     String email,
