5046b0e7d06791532523ed3d468ea603b2e9c50f
ester.oliveira <oliver.ester.2004@gmail.com>
2026-05-22 10:47:40 -0300

    :tada: novo projeto de autenticação com keycloak

---
+++ auth-keycloak/src/main/java/br/com/esteroliver/auth_keycloak/AuthKeycloakApplication.java
@@ -0,0 +1,13 @@
+package br.com.esteroliver.auth_keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthKeycloakApplication.class, args);
+	}
+
+}
---
+++ auth-keycloak/src/test/java/br/com/esteroliver/auth_keycloak/AuthKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.esteroliver.auth_keycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
