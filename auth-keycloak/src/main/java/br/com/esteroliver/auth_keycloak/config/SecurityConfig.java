64681a9a1c8bac5335ee75a25e4ecd87f9be0e08
ester.oliveira <oliver.ester.2004@gmail.com>
2026-07-03 12:27:50 -0300

    feat

--- auth-keycloak/src/main/java/br/com/esteroliver/auth_keycloak/config/SecurityConfig.java
+++ auth-keycloak/src/main/java/br/com/esteroliver/auth_keycloak/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package br.com.esteroliver.auth_keycloak.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,16 +13,24 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Autowired
     KeycloakProperties keycloakProperties;
+
+    private final String[] URLS_CORS_ENABLES = {
+            "http://localhost:4200"
+    };
 
     private final String[] ENDPOINTS_PUBLICOS = {
             "/auth/**",
@@ -31,43 +40,56 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-         http
+         return http
              .csrf(AbstractHttpConfigurer::disable)
              .sessionManagement((session) ->
                  session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
              )
              .authorizeHttpRequests((auth) -> auth
                      .requestMatchers(ENDPOINTS_PUBLICOS).permitAll()
-                     .anyRequest().authenticated()
+                     .anyRequest().permitAll()
              )
              .oauth2ResourceServer((oauth2) ->
                      oauth2.jwt(jwt ->
                              jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
                      )
-             );
-        
-        return http.build();
+             )
+             .build();
     }
 
     @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter(){
-        var converter = new JwtAuthenticationConverter();
-
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
         converter.setJwtGrantedAuthoritiesConverter(jwt -> {
             Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
             if(resourceAccess == null)
                 return List.of();
 
-            var clientAccess = (Map<String, Object>) resourceAccess.get("");
-            if (clientAccess == null) return List.of();
+            var clientAccess = (Map<String, Object>) resourceAccess.get(keycloakProperties.getClientId());
+            if (clientAccess == null)
+                return List.of();
 
             var roles = (List<String>) clientAccess.get("roles");
             return roles.stream()
                     .map(role -> (GrantedAuthority) new SimpleGrantedAuthority("ROLE_" + role))
                     .toList();
         });
-
         return converter;
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+        CorsConfiguration conf = new CorsConfiguration();
+
+        conf.setAllowedOrigins(List.of(URLS_CORS_ENABLES));
+        conf.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        conf.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        conf.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", conf);
+
+        return source;
     }
 
 }
--- auth-keycloak/src/main/java/br/com/esteroliver/auth_keycloak/controllers/ClienteController.java
+++ auth-keycloak/src/main/java/br/com/esteroliver/auth_keycloak/controllers/ClienteController.java
@@ -4,6 +4,7 @@ import br.com.esteroliver.auth_keycloak.dto.UsuarioResponseDTO;
 import br.com.esteroliver.auth_keycloak.entity.Usuario;
 import br.com.esteroliver.auth_keycloak.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cliente")
+@PreAuthorize("hasRole('CLIENTE')")
 public class ClienteController {
 
     @Autowired
@@ -21,6 +23,11 @@ public class ClienteController {
         String keycloakId = authToken.getToken().getSubject();
         Usuario usuario = usuarioRepository.findByKeycloakId(keycloakId).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
         return UsuarioResponseDTO.from(usuario);
+    }
+
+    @GetMapping
+    public String hello(){
+        return "Hello World!";
     }
 
 }
